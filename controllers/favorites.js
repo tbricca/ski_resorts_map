da565531b6f5d1389c0686544edffadd03028ba4
tbricca <tbricca@gmail.com>
2017-10-08 21:59:07 -0700

    added animation

--- controllers/favorites.js
+++ controllers/favorites.js
@@ -7,27 +7,43 @@ var isLoggedIn = require('../middleware/isLoggedIn');
 
 
 router.post('/', function(req, res) {
-  db.favorite.findOrCreate({
-    where: { lat: req.body.lat, lng:req.body.lng},
-    defaults: {
-      name: req.body.name,
-      info: req.body.info,
-      userId: req.session.user
-    }
-  }).spread(function(user, created) {
-    if (created) {
-      passport.authenticate('local', {
-          successRedirect: '/',
-          successFlash: 'Added to Favorites'
-        })(req, res);
-      } else {
-        // if not created, the email already exists
-        req.flash('error', 'You need to login to do that');
-        res.redirect('/auth/signup');
+  if (req.session.user){
+    db.favorite.findOrCreate({
+      where: { lat: req.body.lat, lng:req.body.lng},
+      defaults: {
+        name: req.body.name,
+        info: req.body.info,
+        userId: req.session.user
       }
-    })
+    }).spread(function(user, created) {
+      if (created) {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            successFlash: 'Added to Favorites'
+          })(req, res);
+        } else {
+          // if not created, the email already exists
+          req.flash('error', 'You need to login to do that');
+          res.redirect('/auth/signup');
+        }
+      })
+  }
+
   })
 
+  // router.get('/', function(req, res) {
+  //   db.favorite.findAll({
+  //     where: {userId: req.session.user}
+  //   })
+  //   .then req.render(favorite){
+  //
+  //   }
+  // })
+  // router.delete('/',function(req,res){
+  //   de.favorite.delete({
+  //     where:
+  //   })
+  // })
 
 
 
